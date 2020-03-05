761f79d73118d5482295f6def775e954e28158ab
Gao Jia <89647127@qq.com>
2020-03-04 16:17:07 -0800

    Location changed

--- aau/wnm617/m09/demo/js/pages.js
+++
@@ -1,9 +0,0 @@
-// Page Show Functions
-
-const showListPage = () => {
-	if(!waitForDB(showListPage)) return;
-
-	// console.log("Page Loading",db)
-	$("#page-list [data-role='main']")
-		.html(makeAnimalList(currentUser().animals));
-}
